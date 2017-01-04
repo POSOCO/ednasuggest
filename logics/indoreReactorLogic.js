25f89ded69636ccf38292637257d51e4ff09890e
WRLDC <wrldcposoco@gmail.com>
2017-01-04 12:22:19 +0530

    accommodated both the reactors in logic

--- logics/indoreReactorLogic.js
+++ logics/indoreReactorLogic.js
@@ -1,16 +1,18 @@
 (function (provider, done) {
     var indoreReactorFlowPoint = "WRLDCMP.SCADA1.A0001682";
+    var indoreReactorFlowPoint_2 = "WRLDCMP.SCADA1.A0001684";
     var indoreBusVoltagePoint = "WRLDCMP.SCADA1.A0001653";
-    provider.eDNARealPointFetcher([indoreBusVoltagePoint, indoreReactorFlowPoint], function (err, resultsArray) {
+    provider.eDNARealPointFetcher([indoreBusVoltagePoint, indoreReactorFlowPoint, indoreReactorFlowPoint_2], function (err, resultsArray) {
         if (err) {
             done(null);
         }
         //run the logic now
         var indoreBusVolt = resultsArray[0];
         var indoreReactorFlow = resultsArray[1];
-        if (indoreBusVolt < 760 && indoreReactorFlow > 5) {
+        var indoreReactorFlow_2 = resultsArray[2];
+        if (indoreBusVolt < 760 && indoreReactorFlow > 5 && indoreReactorFlow_2 > 5) {
             done("Indore 765 B/R can be taken out since voltage at Indore is " + indoreBusVolt);
-        } else if (indoreBusVolt > 780 && indoreReactorFlow < 5) {
+        } else if (indoreBusVolt > 780 && indoreReactorFlow < 5 && indoreReactorFlow_2 < 5) {
             done("Indore 765 B/R can be taken into service since voltage at Indore is " + indoreBusVolt);
         } else{
             done(null);
